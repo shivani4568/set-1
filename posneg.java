acca18269f7d4b7a4991e0fcb0a1d35196ac6aa5
shivani4568 <45173031+shivani4568@users.noreply.github.com>
2018-11-19 20:06:20 +0530

    Create posneg.java

---
+++ posneg.java
@@ -0,0 +1,16 @@
+public class even
+{
+public static void main(String args[])
+{
+int n=3;
+if(n>0){
+    System.out.println("Postive");
+}
+else if(n<0)
+ {   System.out.println("Negative");
+
+}
+else
+    System.out.println("Zero");
+}
+}
