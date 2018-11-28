de229b79315912869ebe7025bb13f2e1b856dd7a
shivani4568 <45173031+shivani4568@users.noreply.github.com>
2018-11-28 20:12:55 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,13 @@
+public class vowelcons
+{
+public static void main(String []args)
+{
+char ch='$';
+if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
+System.out.println("Vowel");
+else if((ch>'a' && ch<'z') || (ch>'A' && ch<'Z'))
+System.out.println("Consonant");
+else
+System.out.println("Invalid");
+}
+}
